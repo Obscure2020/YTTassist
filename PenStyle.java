0d4ba6f876b480ae6cc4a31ce0000b73944f2acf
Obscure2020 <jas.elgoog@gmail.com>
2022-06-06 18:31:47 -0400

    Alpha WindowPosition and PenStyle

---
+++ PenStyle.java
@@ -0,0 +1,100 @@
+import java.util.InputMismatchException;
+
+public class PenStyle {
+
+    private boolean bold;
+    private boolean italics;
+    private boolean underline;
+    private int fontColor;
+
+    public PenStyle(){ //Set all values to default state
+        bold = false;
+        italics = false;
+        underline = false;
+        fontColor = -1;
+    }
+
+    public PenStyle(PenStyle other){ //Copy over all values
+        bold = other.bold;
+        italics = other.italics;
+        underline = other.underline;
+        fontColor = other.fontColor;
+    }
+
+    public boolean equals(PenStyle other){
+        if(bold != other.bold) return false;
+        if(italics != other.italics) return false;
+        if(underline != other.underline) return false;
+        if(fontColor != other.fontColor) return false;
+        return true;
+    }
+
+    public boolean isDefault(){ //If text is to use default pen styling, a pen isn't actually necessary. This is the check for unnecessarity.
+        if(bold) return false;
+        if(italics) return false;
+        if(underline) return false;
+        if(fontColor != -1) return false;
+        return true;
+    }
+
+    public String printMarkup(int index){
+        StringBuilder sb = new StringBuilder("<pen id=\"");
+        sb.append(index);
+        sb.append('"');
+        if(bold) sb.append(" b=\"1\"");
+        if(italics) sb.append(" i=\"1\"");
+        if(underline) sb.append(" u=\"1\"");
+        if(fontColor != -1){
+            sb.append(" fc=\"#");
+            sb.append(Integer.toString(fontColor, 16).toUpperCase());
+            sb.append('"');
+        }
+        sb.append("/>");
+        return sb.toString();
+    }
+
+    public void setBold(){
+        bold = true;
+    }
+
+    public void resetBold(){
+        bold = false;
+    }
+
+    public void setItalics(){
+        italics = true;
+    }
+
+    public void resetItalics(){
+        italics = false;
+    }
+
+    public void setUnderline(){
+        underline = true;
+    }
+
+    public void resetUnderline(){
+        underline = false;
+    }
+
+    public void setFontColor(String input) throws InputMismatchException, NumberFormatException{
+        if(input.length() != 6){
+            StringBuilder sb = new StringBuilder("\"");
+            sb.append(input);
+            sb.append("\" is not a valid Color. Colors must be six hexadecimal digits.");
+            throw new InputMismatchException(sb.toString());
+        }
+        int newColor = Integer.parseInt(input, 16);
+        if(newColor < 0){
+            StringBuilder sb = new StringBuilder("\"");
+            sb.append(input);
+            sb.append("\" is not a valid Color. Colors must be between 000000 and FFFFFF.");
+            throw new InputMismatchException(sb.toString());
+        }
+        fontColor = newColor;
+    }
+
+    public void resetFontColor(){
+        fontColor = -1;
+    }
+}
---
+++ WindowPosition.java
@@ -0,0 +1,77 @@
+public class WindowPosition {
+
+    private int anchorPosition;
+    // 0 - Top Left    | 1 - Top Center    | 2 - Top Right
+    // 3 - Center Left | 4 - True Center   | 5 - Center Right
+    // 6 - Bottom Left | 7 - Bottom Center | 8 - Bottom Right
+    private int alignHorizontal; // Percentage from left
+    private int alignVertical; // Percentage from top
+
+    public WindowPosition(){ //Set all values to default state
+        anchorPosition = 7;
+        alignHorizontal = 50;
+        alignVertical = 100;
+    }
+
+    public WindowPosition(WindowPosition other){ //Copy over all values
+        anchorPosition = other.anchorPosition;
+        alignHorizontal = other.alignHorizontal;
+        alignVertical = other.alignVertical;
+    }
+
+    public boolean equals(WindowPosition other){
+        if(anchorPosition != other.anchorPosition) return false;
+        if(alignHorizontal != other.alignHorizontal) return false;
+        if(alignVertical != other.alignVertical) return false;
+        return true;
+    }
+
+    public String printMarkup(int index){
+        StringBuilder sb = new StringBuilder("<wp id=\"");
+        sb.append(index);
+        sb.append("\" ap=\"");
+        sb.append(anchorPosition);
+        sb.append("\" ah=\"");
+        sb.append(alignHorizontal);
+        sb.append("\" av=\"");
+        sb.append(alignVertical);
+        sb.append("\"/>");
+        return sb.toString();
+    }
+
+    public void setAnchorPosition(int input) throws IndexOutOfBoundsException {
+        if(input < 0 || input > 8){
+            StringBuilder sb = new StringBuilder("\"");
+            sb.append(input);
+            sb.append("\" is not a valid Anchor Position. ");
+            if(input < 0){
+                sb.append("Minimum value is 0 (Top Left).");
+            } else {
+                sb.append("Maximum value is 8 (Bottom Right).");
+            }
+            throw new IndexOutOfBoundsException(sb.toString());
+        }
+        anchorPosition = input;
+    }
+
+    public void resetAnchorPosition(){
+        anchorPosition = 7;
+    }
+
+    public void setAlignHorizontal(int input){
+        alignHorizontal = input;
+    }
+
+    public void resetAlignHorizontal(){
+        alignHorizontal = 50;
+    }
+
+    public void setAlignVertical(int input){
+        alignVertical = input;
+    }
+
+    public void resetAlignVertical(){
+        alignVertical = 100;
+    }
+
+}
