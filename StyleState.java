d353987b93909f907db470ad36c8605535dec6ce
Obscure2020 <jas.elgoog@gmail.com>
2022-06-07 23:57:47 -0400

    Alpha StyleState

---
+++ StyleState.java
@@ -0,0 +1,23 @@
+public class StyleState {
+
+    private WindowPosition wp;
+    private PenStyle pen;
+
+    public StyleState(){ //Initialize members to default settings
+        wp = new WindowPosition();
+        pen = new PenStyle();
+    }
+
+    public StyleState(StyleState other){ //Copy over settings of members
+        wp = new WindowPosition(other.wp);
+        pen = new PenStyle(other.pen);
+    }
+
+    public WindowPosition windowPosition(){ //Expose wp for use
+        return wp;
+    }
+
+    public PenStyle penStyle(){ //Expose pen for use
+        return pen;
+    }
+}
